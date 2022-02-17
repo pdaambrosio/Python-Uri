ce0f73846a2b46db79d2c95fbdc17900406881de
Paulo Daniel A Santos <pda.ambrosio@gmail.com>
2022-02-17 08:08:28 -0300

    issue code: no-else-return

--- Subsequences Contiguous/uri2126.py
+++ Subsequences Contiguous/uri2126.py
@@ -3,9 +3,8 @@ def searching_subsequences(seq1: str, seq2: str) -> str:
 
     if amount == 0:
         return 'Nao existe subsequencia\n'
-    else:
-        position: int = seq2.rfind(seq1)
-        return f'Qtd.Subsequencias: {amount}\nPos: {position + 1}\n'
+    position: int = seq2.rfind(seq1)
+    return f'Qtd.Subsequencias: {amount}\nPos: {position + 1}\n'
 
 
 def main():
