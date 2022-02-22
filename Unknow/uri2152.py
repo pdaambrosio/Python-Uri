b395294f4a23ef5166255d6c9584e9c0a81b385b
Paulo Daniel A Santos <pda.ambrosio@gmail.com>
2022-02-21 22:58:31 -0300

    uri 2152

---
+++ Unknow/uri2152.py
@@ -0,0 +1,18 @@
+def already_took_the_candle(hour: int, minute: int, port: int) -> str:
+    occurrence_recorded: str = ('A porta abriu!' if port == 1 else 'A porta fechou!')
+    return f'{hour:02}:{minute:02} - {occurrence_recorded}'
+
+
+def main() -> None:
+    loop: int = int(input())
+    m: int
+    n: int
+    o: int
+
+    for i in range(loop):
+        [m, n, o] = map(int, input().split())
+        print(already_took_the_candle(m, n, o))
+
+
+if __name__ == '__main__':
+    main()    
