7ec3597a48d34b2901ed8400adb445873fc3ad5b
Paulo Daniel A Santos <pda.ambrosio@gmail.com>
2022-03-04 09:01:35 -0300

    uri 2163 Wrong answer (30%)

--- Unknow/uri2163.py
+++ Unknow/uri2163.py
@@ -1,20 +1,58 @@
-n: int
-m: int
-n, m = map(int, input().split())
-
-def force_awakens(lines, columns):
+def force_awakens(lines: int, columns: int) -> int:
     terrain: list[list[int]] = []
     
     for line in range(lines):
         terrain.append([int(x) for x in input().split()])
 
-    for line, column in enumerate(terrain):
-        if 42 in column:
-            if column.index(42) + 1 == 7:
-                return '0 0'
-            else:
-                return f'{line + 1} {column.index(42) + 1}'
+    l: int = 0
+    c: int = 0
 
-print(force_awakens(n, m))
+    for i in range(1, lines - 1):
+        for j in range(1, columns - 1):
+            if terrain[i][j] == 42:
+                if terrain[i-1][j-1] == 7 and terrain[i-1][j] == 7 and terrain[i-1][j+1] == 7:
+                    if terrain[i][j-1] == 7 and terrain[i][j+1] == 7:
+                        if terrain[i+1][j-1] == 7 and terrain[i+1][j] == 7 and terrain[i+1][j+1] == 7:
+                            l = i + 1
+                            c = j+ 1
+                            return f'{c} {l}'
+        return f'{c} {l}'
 
-# TODO: Test the function with other values and create the main function for execution
+
+def main() -> None:
+    n: int
+    m: int
+    n, m = map(int, input().split())
+    print(force_awakens(n, m))
+    def force_awakens(lines: int, columns: int) -> str:
+    terrain: list[list[int]] = []
+
+    for line in range(lines):
+        terrain.append([int(x) for x in input().split()])
+
+    l: int = 0
+    c: int = 0
+
+    for i in range(1, lines - 1):
+        for j in range(1, columns - 1):
+            if terrain[i][j] == 42:
+                if terrain[i - 1][j - 1] == 7 and terrain[i - 1][j] == 7 and terrain[i - 1][j + 1] == 7:
+                    if terrain[i][j - 1] == 7 and terrain[i][j + 1] == 7:
+                        if terrain[i + 1][j - 1] == 7 and terrain[i + 1][j] == 7 and terrain[i + 1][j + 1] == 7:
+                            l = i + 1
+                            c = j + 1
+                            return f'{c} {l}'
+        return f'{c} {l}'
+
+
+def main() -> None:
+    n: int
+    m: int
+    n, m = map(int, input().split())
+    print(force_awakens(n, m))
+
+
+if __name__ == '__main__':
+    main()
+
+# TODO: Wrong answer (30%)
