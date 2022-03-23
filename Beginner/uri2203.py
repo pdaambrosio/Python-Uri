083643f271f6e45d1d6be854808169bac8707257
Paulo Daniel A Santos <pda.ambrosio@gmail.com>
2022-03-23 14:36:58 -0300

    uri 2203

---
+++ Beginner/uri2203.py
@@ -0,0 +1,20 @@
+def crowstorm_lol(list_args: list) -> str:
+    from math import sqrt, pow
+    [xf, yf, xi, yi, vi, r1, r2] = list(map(int, list_args))
+    distance: float = sqrt(pow((xf - xi), 2) + pow((yf - yi), 2))
+
+    if r1 + r2 >= distance + 1.5 * vi:
+        return 'Y'
+    else:
+        return 'N'
+
+def main() -> None:
+    while True:
+        try:
+            list_args: list[str] = input().split()
+            print(crowstorm_lol(list_args))
+        except EOFError:
+            break
+
+if __name__ == '__main__':
+    main()
