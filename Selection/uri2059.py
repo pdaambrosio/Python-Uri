191ec29296f14abe510500d1ec1d8d8e0e708d21
Paulo Daniel A Santos <pda.ambrosio@gmail.com>
2022-03-23 15:10:42 -0300

    fix: string literals should not be duplicated

--- Selection/uri2059.py
+++ Selection/uri2059.py
@@ -1,25 +1,29 @@
-def odd_even_cheat(choice_player1, number_j1, number_j2, cheat_player1, accuse_player2):
+def odd_even_cheat(choice_player1: int, number_j1: int, number_j2: int, cheat_player1: int, accuse_player2: int) -> str:
+    player_one_win: str = 'Jogador 1 ganha'
+    player_two_win: str = 'Jogador 2 ganha'
+
     if cheat_player1 == 1 and accuse_player2 == 1:
-        return 'Jogador 2 ganha!'
+        return player_two_win
 
     if cheat_player1 == 1 or accuse_player2 == 1:
-        return 'Jogador 1 ganha!'
+        return player_one_win
 
     odd_even = (number_j1 + number_j2) % 2
     
     if odd_even == 0:
         if choice_player1 == 1:
-            return 'Jogador 1 ganha!'
-        return 'Jogador 2 ganha!'
+            return player_one_win
+        return player_two_win
 
     if odd_even == choice_player1:
-        return 'Jogador 2 ganha!'
-    return 'Jogador 1 ganha!'
+        return player_two_win
+    return player_one_win
 
 
-def main():
+def main() -> None:
     [p, j1, j2, r, a] = map(int, input().split())
     print(odd_even_cheat(p, j1, j2, r, a))
 
 
-main()
+if __name__ == '__main__':
+    main()
