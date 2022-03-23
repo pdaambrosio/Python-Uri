8927cb0a0d55f57645d3127e61ead1450b213d44
Paulo Daniel A Santos <pda.ambrosio@gmail.com>
2022-03-23 15:04:33 -0300

    fix: string literals should not be duplicated

--- Selection/uri1051.py
+++ Selection/uri1051.py
@@ -1,13 +1,15 @@
-salary = float(input())
+salary: float = float(input())
+taxes: float = 0
+result: str = 'R$ {:.2f}'
 
 if salary <= 2000:
     print('Isento')
 elif salary <= 3000:
     taxes = (salary - 2000) * 0.08
-    print('R$ {:.2f}'.format(taxes))
+    print(result.format(taxes))
 elif salary <= 4500:
     taxes = ((salary - 3000) * 0.18) + (1000 * 0.08)
-    print('R$ {:.2f}'.format(taxes))
+    print(result.format(taxes))
 else:
     taxes = ((salary - 4500) * 0.28) + (1500 * 0.18) + (1000 * 0.08)
-    print('R$ {:.2f}'.format(taxes))
+    print(result.format(taxes))
