7e0d6f5d7890dab3a076f609cd147b1c4e8bcc73
Paulo Daniel A Santos <pda.ambrosio@gmail.com>
2022-05-05 08:30:47 -0300

    uri 2502 refactor

--- Unknow/uri2502.py
+++ Unknow/uri2502.py
@@ -1,22 +1,24 @@
-def deciphering_encrypted_card(number_of_letters: int, loop: int, key_one: str, key_two: str):
+def deciphering_encrypted_card(loop: int, key_one: str, key_two: str) -> str:
     key_one_lower: str = key_one.lower()
     key_two_lower: str = key_two.lower()
     key_one_upper: str = key_one.upper()
     key_two_upper: str = key_two.upper()
+    result: str = ''
     for _ in range(loop):
         text_encrypted: str = input()
         for caracter in text_encrypted:
             if caracter in key_one_lower:
-                return f'{key_two_lower[key_one_lower.index(caracter)]}\b'
+                result += key_two_lower[key_one_lower.index(caracter)]
             elif caracter in key_two_lower:
-                return f'{key_one_lower[key_two_lower.index(caracter)]}\b'
-
-            if caracter in key_one_upper:
-                return f'{key_two_upper[key_one_upper.index(caracter)]}\b'
+                result += key_one_lower[key_two_lower.index(caracter)]
+            elif caracter in key_one_upper:
+                result += key_two_upper[key_one_upper.index(caracter)]
             elif caracter in key_two_upper:
-                return f'{key_one_upper[key_two_upper.index(caracter)]}\b'
-            
-            return f'{caracter}\b'
+                result += key_one_upper[key_two_upper.index(caracter)]
+            else:
+                result += caracter
+        result += '\n'
+    return f'{result}'
 
 
 def main() -> None:
@@ -27,7 +29,7 @@ def main() -> None:
             [c, n] = map(int, input().split())
             k_one = input()
             k_two = input()
-            print(deciphering_encrypted_card(c, n, k_one, k_two))
+            print(deciphering_encrypted_card(n, k_one, k_two))
         except EOFError:
             break
 
