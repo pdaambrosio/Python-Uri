dc54f640a6bb000b6f69b473a996a06451a21b58
Paulo Daniel A Santos <pda.ambrosio@gmail.com>
2022-05-11 15:06:54 -0300

    uri 2510

---
+++ Unknow/uri2510.py
@@ -0,0 +1,15 @@
+def batmain(villain: str) -> str:
+    if villain == "Superman":
+        return "Why So Serious?"
+    return 'Y'
+
+
+def main() -> None:
+    t: int = int(input())
+    for _ in range(t):
+        villain: str = input()
+        print(batmain(villain))
+
+
+if '__main__' == __name__:
+    main()
