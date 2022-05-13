144a8f6a31736043c72702e9408b4620dc95af9b
Paulo Daniel A Santos <pda.ambrosio@gmail.com>
2022-05-13 08:19:55 -0300

    uri 2520 function city_analogimon

---
+++ Unknow/uri2520.py
@@ -0,0 +1,11 @@
+def city_analogimon(rows: int, columns: int) -> list:
+    """
+    Function that returns a list with describing the city Analógimôn
+    """
+    matrix: list = []
+    for _ in range(rows):
+        values = list(map(int, input().split()))
+        matrix.append(values)
+    return matrix
+
+print(city_analogimon(4, 4))
