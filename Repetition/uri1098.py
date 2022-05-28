5e6483425354085684da6dbdc1e102a340a924bb
Paulo Daniel A Santos <pda.ambrosio@gmail.com>
2022-05-28 09:39:03 -0300

    refact:  ambiguous variable name and type hint

--- Repetition/uri1098.py
+++ Repetition/uri1098.py
@@ -1,14 +1,14 @@
-I = 0
-J = 1
+value_i: int|float = 0
+value_j: int|float = 1
 
-while I <= 2:
-    for l in range(3):
-        if I > 2.19:
-            print('I={:.0f} J={:.0f}'.format(2, J))
-        elif I == 1.0 or I == 0.0 or I > 1.8:
-            print('I={:.0f} J={:.0f}'.format(I, J))
+while value_i <= 2:
+    for _ in range(3):
+        if value_i > 2.19:
+            print('I={:.0f} J={:.0f}'.format(2, value_j))
+        elif value_i == 1.0 or value_i == 0.0 or value_i > 1.8:
+            print('I={:.0f} J={:.0f}'.format(value_i, value_j))
         else:
-            print('I={:.1f} J={:.1f}'.format(I, J))
-        J += 1
-    I += 0.2
-    J = I + 1
+            print('I={:.1f} J={:.1f}'.format(value_i, value_j))
+        value_j += 1
+    value_i += 0.2
+    value_j = value_i + 1
