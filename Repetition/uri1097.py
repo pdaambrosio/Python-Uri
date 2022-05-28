aeaa962128eb029d0480571a2fe8b831b29de74e
Paulo Daniel A Santos <pda.ambrosio@gmail.com>
2022-05-28 09:01:18 -0300

    refact:  ambiguous variable name

--- Repetition/uri1097.py
+++ Repetition/uri1097.py
@@ -1,10 +1,10 @@
-I = 1
-J = 7
+value_i: int = 1
+value_j: int = 7
 
-while I <= 9:
-    for l in range(3):
-        print('I={} J={}'.format(I, J))
-        J -= 1
-    I += 2
-    J += 5
+while value_i <= 9:
+    for _ in range(3):
+        print('I={} J={}'.format(value_i, value_j))
+        value_j -= 1
+    value_i += 2
+    value_j += 5
     
