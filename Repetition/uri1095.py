2e6c624589d8e42f9ebc15b4e7c7975efd10a71b
Paulo Daniel A Santos <pda.ambrosio@gmail.com>
2022-05-28 09:40:57 -0300

    refact:  ambiguous variable name and type hint

--- Repetition/uri1095.py
+++ Repetition/uri1095.py
@@ -1,7 +1,7 @@
-I = 1
-J = 60
+value_i: int = 1
+value_j: int = 60
 
-while J >= 0:
-    print('I = {} J = {}'.format(I, J))
-    I += 3
-    J -= 5
+while value_j >= 0:
+    print('I = {} J = {}'.format(value_i, value_j))
+    value_i += 3
+    value_j -= 5
