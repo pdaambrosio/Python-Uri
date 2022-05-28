0ff2f627d77f3ea6b6b9db6b16357ffb65d6ccd6
Paulo Daniel A Santos <pda.ambrosio@gmail.com>
2022-05-28 08:59:08 -0300

    fix:  variable j

--- Repetition/uri1096.py
+++ Repetition/uri1096.py
@@ -4,6 +4,6 @@ value_j = 7
 while value_i <= 9:
     while value_j >= 5:
         print('I={} J={}'.format(value_i, value_j))
-        j -= 1
+        value_j -= 1
     value_i += 2
     value_j = 7
