48a1fd3a48a9636af865806e1f480d3194b0f8bc
Paulo Daniel A Santos <pda.ambrosio@gmail.com>
2022-07-31 20:27:21 -0300

    uri 2552 test function

--- Matrices/uri2552.py
+++ Matrices/uri2552.py
@@ -1,26 +1,28 @@
 def cheese_bread_sweeper(n_lines: int, m_columns: int) -> list:
     matrix: list = []
     result: list = []
-    count: int = 0
+    # count: int = 0
 
     for _ in range(m_columns):
         matrix.append(list(map(int, input().split())))
 
     for i in range(n_lines):
+        count: int = 0
         result.append([])
         for j in range(m_columns):
             if matrix[i][j] == 1:
                 result[i].append(9)
             elif i > 0 and matrix[i - 1][j] == 1:
-                result[i].append(count)
                 count += 1
+                result[i].append(count)
             elif i < n_lines - 1 and matrix[i + 1][j] == 1:
-                result[i].append(count)
                 count += 1
+                result[i].append(count)
             elif j > 0 and matrix[i][j - 1] == 1:
-                result[i].append(count)
                 count += 1
+                result[i].append(count)
             elif j < m_columns - 1 and matrix[i][j + 1] == 1:
+                count += 1
                 result[i].append(count)
                 
     
