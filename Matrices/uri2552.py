c5ce86e558c7d523bf308f745a33c80770ea34e2
Paulo Daniel A Santos <pda.ambrosio@gmail.com>
2022-08-01 08:38:20 -0300

    uri 2552 function refactoring test ok

--- Matrices/uri2552.py
+++ Matrices/uri2552.py
@@ -1,32 +1,30 @@
 def cheese_bread_sweeper(n_lines: int, m_columns: int) -> list:
     matrix: list = []
-    result: list = []
-    # count: int = 0
 
     for _ in range(m_columns):
         matrix.append(list(map(int, input().split())))
 
-    for i in range(n_lines):
-        count: int = 0
-        result.append([])
-        for j in range(m_columns):
-            if matrix[i][j] == 1:
-                result[i].append(9)
-            elif i > 0 and matrix[i - 1][j] == 1:
-                count += 1
-                result[i].append(count)
-            elif i < n_lines - 1 and matrix[i + 1][j] == 1:
-                count += 1
-                result[i].append(count)
-            elif j > 0 and matrix[i][j - 1] == 1:
-                count += 1
-                result[i].append(count)
-            elif j < m_columns - 1 and matrix[i][j + 1] == 1:
-                count += 1
-                result[i].append(count)
-                
+    for line in range(n_lines):
+        for column in range(m_columns):
+            if matrix[line][column] == 1:
+                matrix[line][column] = 9
+
+    count: int = 0
+    for line in range(n_lines):
+        for column in range(m_columns):
+            if matrix[line][column] != 9:
+                if line > 0 and matrix[line - 1][column] == 9:
+                    count += 1
+                if line < n_lines - 1 and matrix[line + 1][column] == 9:
+                    count += 1
+                if column > 0 and matrix[line][column - 1] == 9:
+                    count += 1
+                if column < m_columns - 1 and matrix[line][column + 1] == 9:
+                    count += 1
+                matrix[line][column] = count
+                count = 0
     
-    return result
+    return matrix
 
 
 print(cheese_bread_sweeper(4, 4))
