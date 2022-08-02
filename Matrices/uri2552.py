02b65977681a912f95f761ffbe48b975c48d8e25
Paulo Daniel A Santos <pda.ambrosio@gmail.com>
2022-08-02 08:07:01 -0300

    uri 2552 new function

--- Matrices/uri2552.py
+++ Matrices/uri2552.py
@@ -9,22 +9,33 @@ def cheese_bread_sweeper(n_lines: int, m_columns: int) -> list:
             if matrix[line][column] == 1:
                 matrix[line][column] = 9
 
+    return matrix
+
+
+def cell_without_cheese_bread(matrix: list, n_lines: int, m_columns) -> list:
     count: int = 0
     for line in range(n_lines):
         for column in range(m_columns):
             if matrix[line][column] != 9:
+
                 if line > 0 and matrix[line - 1][column] == 9:
                     count += 1
+
                 if line < n_lines - 1 and matrix[line + 1][column] == 9:
                     count += 1
+
                 if column > 0 and matrix[line][column - 1] == 9:
                     count += 1
+
                 if column < m_columns - 1 and matrix[line][column + 1] == 9:
                     count += 1
+
                 matrix[line][column] = count
                 count = 0
-    
+
     return matrix
 
 
-print(cheese_bread_sweeper(4, 4))
+
+with_cheese_bread: list = cheese_bread_sweeper(4, 4)
+print(cell_without_cheese_bread(with_cheese_bread, 4, 4))
