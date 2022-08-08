56655aa40a8d0099d1edb037b54c10a5b995cefe
Paulo Daniel A Santos <pda.ambrosio@gmail.com>
2022-08-08 08:38:24 -0300

    uri 2554 test function

--- Beginner/uri2554.py
+++ Beginner/uri2554.py
@@ -2,3 +2,7 @@ def pizza_before_bh(date: str, people: list[int]) -> str:
     if all(i == 1 for i in people):
         return date
     return 'Pizza antes de FdI'
+
+
+print(pizza_before_bh('5/10/2016', [1, 1, 1, 1, 1]))
+print(pizza_before_bh('5/10/2016', [1, 1, 1, 0, 1]))
