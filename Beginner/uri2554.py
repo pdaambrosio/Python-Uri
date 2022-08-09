514a86a76f5f1dda8677d26fbd31e22fd4d274e6
Paulo Daniel A Santos <pda.ambrosio@gmail.com>
2022-08-09 08:14:30 -0300

    test: uri 2554 function main

--- Beginner/uri2554.py
+++ Beginner/uri2554.py
@@ -1,8 +1,27 @@
-def pizza_before_bh(date: str, people: list[int]) -> str:
-    if all(i == 1 for i in people):
+def pizza_before_bh(loop: int, date: str, people: list[int]) -> str:
+    if all(loop == 1 for loop in people):
         return date
     return 'Pizza antes de FdI'
 
 
-print(pizza_before_bh('5/10/2016', [1, 1, 1, 1, 1]))
-print(pizza_before_bh('5/10/2016', [1, 1, 1, 0, 1]))
+def main() -> None:
+    while True:
+        try:
+            n: int
+            d: int
+            [n, d] = list(map(int, input().split()))
+            for _ in range(d):
+                [n_date, d_people] = input().split(' ', 1)
+                d_people = list(map(int, d_people.split()))
+                
+                if pizza_before_bh(n, n_date, d_people) == n_date:
+                    print(n_date)
+                    
+                if pizza_before_bh(n, n_date, d_people) == 'Pizza antes de FdI':
+                    print('Pizza antes de FdI')
+        except EOFError:
+            break
+        
+
+if __name__ == '__main__':
+    main()
