5afa44ac771b0499bd5f0113d45da99e84555083
Paulo Daniel A Santos <pda.ambrosio@gmail.com>
2022-08-12 08:42:23 -0300

    refact: uri 2554 refactored pizza_before_bh function

--- Beginner/uri2554.py
+++ Beginner/uri2554.py
@@ -1,16 +1,18 @@
+from typing import Union
+
 def pizza_before_bh(loop: int) -> str:
     result: str = ''
     for _ in range(loop):
+        n_date: str
+        d_people: Union[str, list]
         [n_date, d_people] = input().split(' ', 1)
         d_people = list(map(int, d_people.split()))
-        
-        if all(i == 1 for i in d_people):
-            result = n_date
-            
-        if len(result) == 0:
-            result = 'Pizza antes de FdI'
 
-    return result
+        if len(result) == 0:
+            if all(i == 1 for i in d_people):
+                result = n_date
+
+    return result if result else 'Pizza antes de FdI'
 
 
 def main() -> None:
