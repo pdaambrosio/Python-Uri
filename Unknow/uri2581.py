be2cdea47eabc2154f9286abe10a58cce50cf5e5
Paulo Daniel A Santos <pda.ambrosio@gmail.com>
2022-08-18 08:09:34 -0300

    uri 2581

--- Unknow/uri2581.py
+++ Unknow/uri2581.py
@@ -1,2 +1,14 @@
 def i_am_toorg(question: str) -> str:
-    return 'I am Toorg!'
+    if question:
+        return 'I am Toorg!'
+
+
+def main() -> None:
+    loop: int = int(input())
+    for _ in range(loop):
+        input_question: str = input()
+        print(i_am_toorg(input_question))
+    
+
+if __name__ == '__main__':
+    main()
