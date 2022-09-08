5af52f69bc89a0867163ca2d7c0a19796beee7b6
Paulo Daniel A Santos <pda.ambrosio@gmail.com>
2022-09-08 08:42:16 -0300

    feature: uri 2632 function init

---
+++ Unknow/uri2632.py
@@ -0,0 +1,9 @@
+def magic_and_sword(width_w: int, height_h:int, coordinate_x0: int, coordinate_y0: int) -> int:
+    spells: dict = {    
+        'fire':  {'dmg': 200, 'l1': 20, 'l2': 30, 'l3': 50},
+        'water': {'dmg': 300, 'l1': 10, 'l2': 25, 'l3': 40},
+        'earth': {'dmg': 400, 'l1': 25, 'l2': 55, 'l3': 70},
+        'air':   {'dmg': 100, 'l1': 18, 'l2': 38, 'l3': 60}
+    }
+    
+    return 0
