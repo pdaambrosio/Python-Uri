70f058e03b6c25467ab99ee5d7f4a387040eafca
Paulo Daniel A Santos <pda.ambrosio@gmail.com>
2022-09-15 08:29:24 -0300

    fix: uri 2632 issue code: E241

--- Unknow/uri2632.py
+++ Unknow/uri2632.py
@@ -2,10 +2,10 @@ def magic_and_sword(width_w: int, height_h:int, coordinate_x0: int, coordinate_y
     from math import sqrt
     
     spells: dict = {    
-        'fire':  {'dmg': 200, 'l1': 20, 'l2': 30, 'l3': 50},
+        'fire': {'dmg': 200, 'l1': 20, 'l2': 30, 'l3': 50},
         'water': {'dmg': 300, 'l1': 10, 'l2': 25, 'l3': 40},
         'earth': {'dmg': 400, 'l1': 25, 'l2': 55, 'l3': 70},
-        'air':   {'dmg': 100, 'l1': 18, 'l2': 38, 'l3': 60}
+        'air': {'dmg': 100, 'l1': 18, 'l2': 38, 'l3': 60}
     }
     
     spell_type: dict = spells[spell]
